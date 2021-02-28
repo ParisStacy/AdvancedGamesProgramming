d0c1e8551ec4402bf52d60633aa43722ed7a9689
ParisStacy <parispluto@gmail.com>
2021-02-28 17:01:53 -0500

    Begun Implementation of checkMove() function for each piece

--- Assets/Week04/Board.cs
+++ Assets/Week04/Board.cs
@@ -2,36 +2,21 @@
 
     public static Piece[] myBoard = new Piece[63];
 
+    public static int[] border = { 0, 1, 2, 3, 4, 5, 6, 7, 8,
+                                  15, 16, 23, 24, 31, 32, 39,
+                                  40, 47, 48, 55, 56, 57, 58,
+                                  59, 60, 61, 62, 63 };
+
     public static void getMoves(Piece piece) {
 
         if (!piece.active) return;
 
-        
+        piece.checkMoves();
+
+    }
+
+    public static void CheckTile(int offset, Piece p) {
 
     }
 }
 
-public class PieceBehavior {
-
-    public delegate void pieceFunction(Piece p);
-
-    public void rookMoves(Piece p) {
-
-    }
-    public void BishopMoves(Piece p) {
-
-    }
-    public void KnightMoves(Piece p) {
-
-    }
-    public void QueenMoves(Piece p) {
-
-    }
-    public void KingMoves(Piece p) {
-
-    }
-    public void PawnMoves(Piece p) {
-
-    }
-
-}
--- Assets/Week04/Classes.cs
+++ Assets/Week04/Classes.cs
@@ -1,16 +1,20 @@
-﻿public class Piece {
-    public bool active;
-    public bool white;
+﻿using System.Linq;
+public abstract class Piece {
+    public abstract bool active { get; set; }
+    public abstract bool white { get; set; }
 
-    public Tile[] availableMoves;
+    public abstract Tile[] availableMoves { get; set; }
 
-    public int position;
-    public string type;
+    public abstract void checkMoves();
+
+    public abstract int position { get; set; }
+    public abstract string type { get; set; }
+
 }
 
 public class Tile {
     public int id;
-    public Piece piece;
+    public int pieceId;
 }
 
 public class Move {
@@ -18,3 +22,121 @@ public class Move {
 }
 
 
+public class Rook : Piece {
+    #region Variables
+    public override bool active { get; set; }
+    public override bool white { get; set; }
+
+    public override Tile[] availableMoves { get; set; }
+
+    public override int position { get; set; }
+    public override string type { get; set; }
+    #endregion
+
+    public override void checkMoves() {
+
+        CheckMovesData d;
+        d._i = 1;
+        d._break = false;
+        d._valid = false;
+
+        //S
+        while(!d._break) {
+
+            if (Board.border.Contains(position + d._i * 8)) d._break = true;
+            if (Board.myBoard[position + d._i * 8].active == true) d._break = true;
+
+            d._valid = !(Board.myBoard[position + d._i * 8].white == white);
+
+            if (d._valid) {
+
+            }
+            
+            d._valid = false;
+            d._i++;
+        }
+
+    }
+}
+
+public class Bishop : Piece {
+    #region Variables
+    public override bool active { get; set; }
+    public override bool white { get; set; }
+
+    public override Tile[] availableMoves { get; set; }
+
+    public override int position { get; set; }
+    public override string type { get; set; }
+    #endregion
+    public override void checkMoves() {
+
+    }
+}
+
+public class Knight : Piece {
+    #region Variables
+    public override bool active { get; set; }
+    public override bool white { get; set; }
+
+    public override Tile[] availableMoves { get; set; }
+
+    public override int position { get; set; }
+    public override string type { get; set; }
+    #endregion
+    public override void checkMoves() {
+        
+    }
+}
+
+public class Queen : Piece {
+    #region Variables
+    public override bool active { get; set; }
+    public override bool white { get; set; }
+
+    public override Tile[] availableMoves { get; set; }
+
+    public override int position { get; set; }
+    public override string type { get; set; }
+    #endregion
+    public override void checkMoves() {
+
+    }
+}
+
+public class King : Piece {
+    #region Variables
+    public override bool active { get; set; }
+    public override bool white { get; set; }
+
+    public override Tile[] availableMoves { get; set; }
+
+    public override int position { get; set; }
+    public override string type { get; set; }
+    #endregion
+    public override void checkMoves() {
+
+    }
+}
+
+public class Pawn : Piece {
+    #region Variables
+    public override bool active { get; set; }
+    public override bool white { get; set; }
+
+    public override Tile[] availableMoves { get; set; }
+
+    public override int position { get; set; }
+    public override string type { get; set; }
+    #endregion
+    public override void checkMoves() {
+
+    }
+}
+
+
+public struct CheckMovesData {
+    public int _i;
+    public bool _break;
+    public bool _valid;
+}
