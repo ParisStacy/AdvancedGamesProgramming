60e2be0e556d908a5e787175716887ce3ad5bfd4
ParisStacy <parispluto@gmail.com>
2021-03-07 16:33:49 -0500

    Reimport

--- Assets/Week5/Week5.cs
+++ Assets/Week5/Week5.cs
@@ -4,13 +4,15 @@ using System.Collections.Generic;
 using System.Globalization;
 using System.Linq;
 using JetBrains.Annotations;
+using SimpleJSON;
 using UnityEngine;
 using TMPro;
 using UnityEngine.Networking;
 using Random = UnityEngine.Random;
 
 [ExecuteInEditMode]
-public class Week5 : MonoBehaviour {
+public class Week5 : MonoBehaviour
+{
     /*
      * Write a CSV parser - that takes in a CSV file of players and returns a list of those players as class objects.
      * To help you out, I've defined the player class below.  An example save file is in the folder "CSV Examples".
@@ -20,8 +22,10 @@ public class Week5 : MonoBehaviour {
      *     - location is tricky - because the location has a comma in it!!
      */
 
-    private class Player {
-        public enum Class : byte {
+    private class Player
+    {
+        public enum Class : byte
+        {
             Undefined = 0,
             Monk,
             Wizard,
@@ -29,7 +33,7 @@ public class Week5 : MonoBehaviour {
             Thief,
             Sorcerer
         }
-
+        
         public Class classType;
         public string name;
         public uint maxHealth;
@@ -38,7 +42,8 @@ public class Week5 : MonoBehaviour {
         public Vector2 location;
     }
 
-    private List<Player> CSVParser(TextAsset toParse) {
+    private List<Player> CSVParser(TextAsset toParse)
+    {
         var toReturn = new List<Player>();
 
         return toReturn;
@@ -55,30 +60,36 @@ public class Week5 : MonoBehaviour {
      * JSON objects.
      */
 
-    public int NumberAboveScore(TextAsset jsonFile, int score) {
+    public int NumberAboveScore(TextAsset jsonFile, int score)
+    {
         var toReturn = 0;
-
+     
         return toReturn;
     }
 
-    public string GetHighScoreName(TextAsset jsonFile) {
+    public string GetHighScoreName(TextAsset jsonFile)
+    {
         return "";
     }
-
+    
     // =========================== DON'T EDIT BELOW THIS LINE =========================== //
 
     public TextMeshProUGUI csvTest, networkTest;
     public TextAsset csvExample, jsonExample;
     private Coroutine checkingScores;
-
-    private void Update() {
+    
+    private void Update()
+    {
         csvTest.text = "CSV Parser\n<align=left>\n";
 
         var parsedPlayers1 = CSVParser(csvExample);
 
-        if (parsedPlayers1.Count == 0) {
+        if (parsedPlayers1.Count == 0)
+        {
             csvTest.text += "Need to return some players.";
-        } else {
+        }
+        else
+        {
             csvTest.text += Success(parsedPlayers1.Any(p => p.name == "Jeff") &&
                                     parsedPlayers1.Any(p => p.name == "Stonks")
                             ) + " read in player names correctly.\n";
@@ -98,13 +109,14 @@ public class Week5 : MonoBehaviour {
                 Success(parsedPlayers1.First(p => p.name == "Fortune").location == new Vector2(12.322f, 42f)) +
                 " Correctly read in location.\n";
         }
-
+        
         networkTest.text = "JSON Data\n<align=left>\n";
         networkTest.text += Success(NumberAboveScore(jsonExample, 10) == 6) + " number above score worked correctly.\n";
         networkTest.text += Success(GetHighScoreName(jsonExample) == "GUW") + " get high score name worked correctly.\n";
     }
-
-    private string Success(bool test) {
+    
+    private string Success(bool test)
+    {
         return test ? "<color=\"green\">PASS</color>" : "<color=\"red\">FAIL</color>";
     }
 }
