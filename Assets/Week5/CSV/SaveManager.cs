748498b449ee077092cf2e1ccd8c490dcf4aa9f5
ParisStacy <parispluto@gmail.com>
2021-03-14 18:13:40 -0400

    Clean Commit - Beginning Problem Sets

---
+++ Assets/Week5/CSV/SaveManager.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+using System;
+
+public class SaveManager
+{
+    public void Initialize() {
+
+    }
+
+    public void loadSave() {
+
+    }
+
+    public void writeSave() {
+
+        //Save Data Categories
+        string[] dataCategories = new string[4];
+
+        dataCategories[0] = "Inventory:";
+        dataCategories[1] = "Save Point Location:";
+        dataCategories[2] = "Health:";
+
+        //Inventory
+
+        //SavePoint
+
+        //Health
+
+
+
+
+    }
+
+}
